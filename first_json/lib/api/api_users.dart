21521493b20befd8e4e65efe327d86595b458c7f
Alessandro <56259457+alemape99@users.noreply.github.com>
2022-04-29 15:52:11 +0200

    lista completa e refresh

--- first_json/lib/api/api_users.dart
+++ first_json/lib/api/api_users.dart
@@ -4,12 +4,17 @@ import 'package:first_json/models/users_response.dart';
 import 'package:http/http.dart' as http;
 
 class ApiUsers{
-  static Future<UserResponse> getUsersFromInternet() async{
-    final response = await http.get(Uri.parse('https://dummyjson.com/users'));
+  static Future<UserResponse> getUsersFromInternet({int skip = 0}) async{
+    final response = await http.get(
+        Uri.parse('https://dummyjson.com/users?skip?$skip'));
 
     if(response.statusCode == 200){
-      return UserResponse.fromJson(jsonDecode(response.body));
+      return UserResponse.fromJson(jsonDecode(response.body)
+      );
     }
-    throw Exception('Error getUsersFromInternet(): ${response.statusCode} - ${response.reasonPhrase}');
+    throw Exception(
+        'Error getUsersFromInternet(): '
+            '${response.statusCode} - '
+            '${response.reasonPhrase}');
   }
 }
--- first_json/lib/home.dart
+++ first_json/lib/home.dart
@@ -1,38 +1,99 @@
 import 'package:first_json/api/api_users.dart';
+import 'package:first_json/models/users.dart';
 import 'package:first_json/models/users_response.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late List<Users> _listaUsersVisualizzate;
+  late bool _hasMoreUsers;
+  late int _skipUsers;
+  late Future<List<Users>> _future;
+
+
+  @override
+  void initState(){
+    super.initState();
+    _listaUsersVisualizzate = [];
+    _hasMoreUsers = false;
+    _skipUsers = 0;
+    _future = _fetchUsers();
+  }
+
+  Future<List<Users>> _fetchUsers() async {
+    final UserResponse result = await ApiUsers.getUsersFromInternet(skip: _skipUsers);
+    setState(() {
+      _skipUsers = _skipUsers + result.limit;
+      _hasMoreUsers = (result.total - _skipUsers > 0);
+      _listaUsersVisualizzate = _listaUsersVisualizzate + result.users;
+    });
+
+    return _listaUsersVisualizzate;
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Users'),
       ),
-      body: FutureBuilder(
-          future: ApiUsers.getUsersFromInternet(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data is UserResponse) {
-              final listUsers = (snapshot.data as UserResponse).users;
+      body: RefreshIndicator(
+        onRefresh: () {
+          setState(() {
+            _listaUsersVisualizzate = [];
+            _hasMoreUsers = false;
+            _skipUsers = 0;
+            _future = _fetchUsers();
+          });
+          return Future.value();
+        },
+        child: FutureBuilder(
+            future: _future,
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data is List<Users>) {
+                final listUsers = snapshot.data as List<Users>;
 
-              return ListView.builder(
-                  itemCount: listUsers.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(listUsers[index].firstName),
-                      subtitle: Text(listUsers[index].lastName),
-                    );
-                  });
-            }
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            }
-            return const Center(child: CircularProgressIndicator(),);
-          }),
+                return ListView.builder(
+                    itemCount: listUsers.length + (_hasMoreUsers ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      if(index == listUsers.length ){
+                        _future = _fetchUsers();
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return ListTile(
+                        title: Text('${listUsers[index].firstName} ${listUsers[index].lastName}'),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(listUsers[index].phone),
+                              Text(listUsers[index].university)
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              }
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(snapshot.error.toString()),
+                );
+              }
+              return const Center(child: CircularProgressIndicator(),);
+            }),
+      ),
     );
   }
 }
