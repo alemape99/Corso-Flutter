ca923441142367a775d7a6c4956986c49f12f414
Alessandro <56259457+alemape99@users.noreply.github.com>
2022-05-02 16:01:14 +0200

    import json

---
+++ social_app/lib/models/tag_response.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'tag_response.g.dart';
+
+
+@JsonSerializable(
+    explicitToJson: true
+)
+class TagResponse {
+  final List<String> data;
+  final int total;
+  final int page;
+  final int limit;
+
+  TagResponse({
+    required this.data,
+    required this.total,
+    required this.page,
+    required this.limit
+  });
+  factory TagResponse.fromJson(Map<String, dynamic> json) => _$TagResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TagResponseToJson(this);
+
+}
