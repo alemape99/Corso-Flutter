4026685b463ddb6a3e804e2aed3b431a27a6849d
Alessandro <56259457+alemape99@users.noreply.github.com>
2022-05-05 17:30:43 +0200

    api per creare post creata

--- social_app/lib/api/api_comment.dart
+++ social_app/lib/api/api_comment.dart
@@ -1,14 +1,17 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:social_app/models/comment.dart';
 import 'package:social_app/models/comment_response.dart';
 
 class ApiComment {
   static String get baseUrl => 'https://dummyapi.io/data/v1';
 
-  static Future<CommentResponse> getUserList({int page = 0, int limit = 20}) async {
-    final response = await http.get(Uri.parse('$baseUrl/comment?page=$page&limit=$limit'), headers: {
+  static Future<CommentResponse> getUserList(
+      {int page = 0, int limit = 20}) async {
+    final response = await http
+        .get(Uri.parse('$baseUrl/comment?page=$page&limit=$limit'), headers: {
       'app-id': '626fc92ee000f64b3bf05f11',
     });
 
@@ -18,9 +21,12 @@ class ApiComment {
     throw Exception('Errore in ricevere gli utenti:'
         '${response.body} ');
   }
-  static Future<CommentResponse> getCommentsFromPost(String id ) async {
-    final response = await http.get(Uri.parse('$baseUrl/post/$id/comment'),
-        headers: {'app-id': '626fc92ee000f64b3bf05f11',});
+
+  static Future<CommentResponse> getCommentsFromPost(String id) async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/post/$id/comment'), headers: {
+      'app-id': '626fc92ee000f64b3bf05f11',
+    });
 
     if (response.statusCode == 200) {
       return CommentResponse.fromJson(jsonDecode(response.body));
@@ -28,14 +34,45 @@ class ApiComment {
     throw Exception('Errore in ricevere gli utenti:'
         '${response.body} ');
   }
+
   static Future<CommentResponse> getCommentsFromUsers(String id) async {
-    final response = await http.get(Uri.parse('$baseUrl/user/$id/comment'),
-        headers: {'app-id': '626fc92ee000f64b3bf05f11',});
+    final response =
+        await http.get(Uri.parse('$baseUrl/user/$id/comment'), headers: {
+      'app-id': '626fc92ee000f64b3bf05f11',
+    });
 
     if (response.statusCode == 200) {
       return CommentResponse.fromJson(jsonDecode(response.body));
     }
     throw Exception('Errore in ricevere gli utenti:'
         '${response.body} ');
+  }
+
+  static Future<Comment> addCommentTo(String postId, String message) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? userId = sp.getString('loggedUserId');
+
+    if (userId == null) {
+      throw Exception(
+          'Impossibile inserire un commento, per favore fai il login');
+    }
+
+    final response = await http.post(Uri.parse('$baseUrl/comment/create'),
+        headers: {
+          'app-id': '626fc92ee000f64b3bf05f11',
+          'Content-Type': 'application/json'
+        },
+        body:
+            jsonEncode({
+              'owner': userId,
+              'post': postId,
+              'message': message}
+            ),
+    );
+    if(response.statusCode == 200){
+      return Comment.fromJson(jsonDecode(response.body));
+    }
+    throw Exception('Commento non inserito:'
+    '${response.body} ');
   }
 }
