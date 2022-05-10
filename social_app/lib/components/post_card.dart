0f4c43aab26b7126d6dde852366428f2b7ae6dcb
Alessandro <56259457+alemape99@users.noreply.github.com>
2022-05-10 16:09:35 +0200

    delete post aggiunto

--- social_app/lib/components/post_card.dart
+++ social_app/lib/components/post_card.dart
@@ -23,7 +23,6 @@ class PostCard extends StatefulWidget {
 
 class _PostCardState extends State<PostCard> {
   String? _userId;
-  late bool _deleted;
 
 
   Future<void> _initIdUtente() async {
@@ -36,169 +35,163 @@ class _PostCardState extends State<PostCard> {
 
   @override
   void initState() {
-    _deleted = false;
+
     _initIdUtente();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Visibility(
-      visible: !_deleted,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.purple),
-              borderRadius: BorderRadius.circular(16)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) =>
-                          ProfilePage(
-                            id: widget.post.owner!.id!,))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 24,
-                            backgroundImage: NetworkImage(widget.post.owner!.picture!,),
-                          ),
-                          const SizedBox(width: 16),
-                          Column(crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                            Text('${widget.post.owner!.firstName} ${widget.post.owner!.lastName}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),),
-                                Row(
-                                  children: [
-                                    if (widget.post.publishDate != null)
-                                      Text(
-                                        DateFormat("d/M/y HH:mm").format(
-                                            DateTime.parse(widget.post.publishDate!)),
-                                      ),
-                                    const SizedBox(
-                                      width: 5,
-                                    ),
-                                    const Icon(Icons.public, color: Colors.purple, size: 15,)
-                                  ],
-                                )
-                          ]),
-                        ],
-                      ),
-                      TextButton(
-                          child: const Text('Modifica'),
-                          onPressed: () async {
-                            var change = await showModalBottomSheet(
-                                context: context,
-                                isScrollControlled: true,
-                                builder: (context) =>
-                                    BottoneAggiungiModificaPost(_userId!, post: widget.post)
-                            );
-                            if (change == true) {
-                              widget.callback();
-                            }
-                          }),
-                      IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.purple,),
-                          onPressed: () async {
-                            late bool result;
-                            if(widget.post.id != null){
-                              result = await ApiPost.deletePost(widget.post.id ?? '');
-                            }
-                            if (result == true) {
-                              setState(() {
-                                _deleted = true;
-                              });
-                            }
-                          }),
-                    ],
-                  ),
-                ),
-                if (widget.post.text != null)
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(widget.post.text ?? ''),
-                  ),
-                const SizedBox(
-                  height: 5,
-                ),
-                if (widget.post.image != null && widget.post.image!.isNotEmpty)
-                  Image.network(
-                    widget.post.image!,
-                  ),
-                const SizedBox(
-                  height: 5,
-                ),
-                if (widget.post.tags != null)
-                  Wrap(
-                    spacing: 6,
-                    children: widget.post.tags!
-                        .map((tag) => Chip(label: Text(tag)))
-                        .toList(),
-                  ),
-                const Divider(
-                  thickness: 2,
-                  color: Colors.purple,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.purple),
+            borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) =>
+                        ProfilePage(
+                          id: widget.post.owner!.id!,))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    if(_userId != null) LikeButton(widget.post, _userId),
-                    if (widget.showCommentButton == true)
-                      TextButton(
-                        onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DetailsPost(
-                                    post: widget.post,
-                                  )),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 24,
+                          backgroundImage: NetworkImage(widget.post.owner!.picture!,),
                         ),
-                        child: Row(
-                          children: const [
-                            Icon(
-                              Icons.mode_comment_outlined,
-                              color: Colors.purple,
-                            ),
-                            SizedBox(
-                              width: 4,
-                            ),
-                            Text('Comment'),
-                          ],
-                        ),
-                        style: TextButton.styleFrom(
-                          primary: Colors.black,
-                        ),
-                      ),
+                        const SizedBox(width: 16),
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                          Text('${widget.post.owner!.firstName} ${widget.post.owner!.lastName}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),),
+                              Row(
+                                children: [
+                                  if (widget.post.publishDate != null)
+                                    Text(
+                                      DateFormat("d/M/y HH:mm").format(
+                                          DateTime.parse(widget.post.publishDate!)),
+                                    ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  const Icon(Icons.public, color: Colors.purple, size: 15,)
+                                ],
+                              )
+                        ]),
+                      ],
+                    ),
                     TextButton(
-                      onPressed: () => print('Share'),
+                        child: const Text('Modifica'),
+                        onPressed: () async {
+                          var change = await showModalBottomSheet(
+                              context: context,
+                              isScrollControlled: true,
+                              builder: (context) =>
+                                  BottoneAggiungiModificaPost(_userId!, post: widget.post)
+                          );
+                          if (change == true) {
+                            widget.callback();
+                          }
+                        }),
+                    IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.purple,),
+                        onPressed: () async {
+                          if(widget.post.id != null){
+                            await ApiPost.deletePost(widget.post.id!);
+                          }
+                            setState(() {
+                              widget.callback();
+                            });
+                        }),
+                  ],
+                ),
+              ),
+              if (widget.post.text != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(widget.post.text ?? ''),
+                ),
+              const SizedBox(
+                height: 5,
+              ),
+              if (widget.post.image != null && widget.post.image!.isNotEmpty)
+                Image.network(
+                  widget.post.image!,
+                ),
+              const SizedBox(
+                height: 5,
+              ),
+              if (widget.post.tags != null)
+                Wrap(
+                  spacing: 6,
+                  children: widget.post.tags!
+                      .map((tag) => Chip(label: Text(tag)))
+                      .toList(),
+                ),
+              const Divider(
+                thickness: 2,
+                color: Colors.purple,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  if(_userId != null) LikeButton(widget.post, _userId),
+                  if (widget.showCommentButton == true)
+                    TextButton(
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DetailsPost(
+                                  post: widget.post,
+                                )),
+                      ),
                       child: Row(
                         children: const [
                           Icon(
-                            Icons.share_outlined,
+                            Icons.mode_comment_outlined,
                             color: Colors.purple,
                           ),
                           SizedBox(
                             width: 4,
                           ),
-                          Text('Share'),
+                          Text('Comment'),
                         ],
                       ),
                       style: TextButton.styleFrom(
                         primary: Colors.black,
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  TextButton(
+                    onPressed: () => print('Share'),
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.share_outlined,
+                          color: Colors.purple,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text('Share'),
+                      ],
+                    ),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
